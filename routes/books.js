b3017a51cd7fee9b80df6842dfec18d3eb2bbb18
Matteo Cilli <cilli.matteo@gmail.com>
2020-12-20 17:40:52 +0100

    Strike Package Bravo

--- routes/books.js
+++ routes/books.js
@@ -20,28 +20,62 @@ router.get('/', asyncHandler(async(req, res) => {
 }));
 
 router.get('/new', asyncHandler(async(req, res) => {
-    const books = await Book.findAll();
-    res.json(books)
+    res.render("books/new", { book: {}, title: "New Book" });
 }));
 
-router.post('/new', asyncHandler(async(req, res) => {
-    const books = await Book.findAll();
-    res.json(books)
+router.post('/', asyncHandler(async(req, res) => {
+    let book;
+    try {
+        book = await Book.create(req.body);
+        res.redirect("/");
+    } catch (error) {
+        if (error.name === "SequelizeValidationError") {
+            book = await Book.build(req.body);
+            res.render("books/new", { book, errors: error.errors, title: "New Article" });
+        } else {
+            throw error;
+        }
+    }
 }));
 
 router.get('/:id', asyncHandler(async(req, res) => {
-    const books = await Book.findAll();
-    res.json(books)
+    const book = await Book.findByPk(req.params.id);
+    if (book) {
+        res.render('books/edit', { book, title: 'Update Book' });
+    } else {
+        res.sendStatus(404);
+    }
 }));
 
 router.post('/:id', asyncHandler(async(req, res) => {
-    const books = await Book.findAll();
-    res.json(books)
+    let book;
+    try {
+        book = await Book.findByPk(req.params.id);
+        if (book) {
+            await book.update(req.body);
+            res.redirect("/");
+        } else {
+            res.sendStatus(404);
+        }
+    } catch (error) {
+        if (error.name === "SequelizeValidationError") {
+            book = await Book.build(req.body);
+            book.id = req.params.id;
+            res.render("books/edit", { book, errors: error.errors, title: "Update Book" });
+        } else {
+            throw error;
+        }
+    }
 }));
 
 router.post('/:id/delete', asyncHandler(async(req, res) => {
-    const books = await Book.findAll();
-    res.json(books)
+    const book = await Book.findByPk(req.params.id);
+    if (book) {
+        await book.destroy();
+        res.redirect("/");
+    } else {
+        res.sendStatus(404);
+    }
 }));
 
 
